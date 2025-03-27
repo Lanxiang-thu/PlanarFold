e348345a098ff34d5b2141f0048da596ff5a4c4a
Lanxiang-thu <xiangl20@mails.tsinghua.edu.cn>
2025-03-27 21:50:55 +0800

    Add files via upload

---
+++ benchmark/thermodynamics_kinetics/bistable55/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# start simulation using ssRNA.inpcrd
+PlanarFold  -i controls.in  -f fparm.in -p ssRNA.prmtop  -c ssRNA.inpcrd \
+  	      -o run001.out  -x run001.nc  -r run001.rst \
+	      -ntr restraint_ntr.in -dpr restraint_dpr.in
+
+# extend simulation using restart file
+istart=2
+iend=100
+I=$istart
+while [ $I -le $iend ]
+do
+  suffix=`printf %03d $I`
+  suffix2=`printf %03d $((I-1))`
+  # production run
+  PlanarFold  -i controls.in  -f fparm.in -p ssRNA.prmtop  -c run$suffix2.rst \
+  	      -o run$suffix.out  -x run$suffix.nc  -r run$suffix.rst \
+	      -ntr restraint_ntr.in -dpr restraint_dpr.in
+  I=$((I+1))
+done
+
+
